bc954cba99efae4f2fd92a2fc4a58b534d78e273
Ryan Biesemeyer <v-ryanbi@microsoft.com>
2011-01-26 13:53:05 -0800

    Make debug tools more versatile

--- pftt-debug.bat
+++ pftt-debug.bat
@@ -1,7 +1,5 @@
 @echo OFF
 echo Generating debug build...
-attrib -R .pftt-debug.js
-cscript /nologo %~dp0\generate-debug.js
-attrib +R .pftt-debug.js
+cscript /nologo %~dp0\tools\generate-debug.js %~dp0\pftt.js
 echo Executing debug build...
-cscript /nologo %~dp0\.pftt-debug.js %*
+cscript /nologo %~dp0\pftt.debug.js %*
